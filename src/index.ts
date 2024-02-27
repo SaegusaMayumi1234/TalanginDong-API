a02329fdcb012c2b0f6488dd2c21da080eaae9f6
SaegusaMayumi1234 <theresanojulianl1234@gmail.com>
2024-02-27 19:31:07 +0700

    feat: Added API init Documentation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import * as logger from './utils/logger';
 import NotFound from './middlewares/notFound';
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app: Express = express();
 const port = process.env.PORT || 3000;
 const proxied = process.env.PROXIED;
@@ -16,7 +20,7 @@ app.set('trust proxy', !proxied || proxied === 'false' || isNaN(parseInt(proxied
 app.use(cors());
 app.use(express.json({ limit: '15mb' }));
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, '/public')));
 app.get('/', (req: Request, res: Response) => {
   res.send('Success');
 });
