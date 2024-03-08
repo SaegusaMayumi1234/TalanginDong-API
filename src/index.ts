20baf036022aee39ae65d81c10f63a06f465e3a0
SaegusaMayumi1234 <theresanojulianl1234@gmail.com>
2024-03-08 08:34:25 +0700

    fix: Changed middleware order

--- src/index.ts
+++ src/index.ts
@@ -29,10 +29,11 @@ const port = process.env.PORT || 3000;
 
   app.set('trust proxy', process.env.PROXIED === 'false' ? false : parseInt(process.env.PROXIED!, 10));
   // app.use(Auth);
+
   app.use(helmet());
-  app.use(cors());
   app.use(express.json({ limit: '15mb' }));
   app.use(express.urlencoded({ extended: true }));
+  app.use(cors());
 
   app.use(express.static(path.join(__dirname, '/public')));
 
