e3274a59936d0f46b956fefa974935a72cc161d1
SaegusaMayumi1234 <theresanojulianl1234@gmail.com>
2024-03-08 08:32:53 +0700

    feat: Added security using helmet deps

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -28,6 +29,7 @@ const port = process.env.PORT || 3000;
 
   app.set('trust proxy', process.env.PROXIED === 'false' ? false : parseInt(process.env.PROXIED!, 10));
   // app.use(Auth);
+  app.use(helmet());
   app.use(cors());
   app.use(express.json({ limit: '15mb' }));
   app.use(express.urlencoded({ extended: true }));
