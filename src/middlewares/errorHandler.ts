f208f5788cf3f0ccce399964f92dd038b94e868e
SaegusaMayumi1234 <theresanojulianl1234@gmail.com>
2024-03-11 10:28:16 +0700

    fix: Handle body-parser error

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -5,11 +5,27 @@ import * as logger from '../utils/logger';
 import config from '../config/config';
 import ApiError from '../utils/apiError';
 
+const bodyParserErrorType = [
+  'encoding.unsupported',
+  'entity.parse.failed',
+  'entity.verify.failed',
+  'request.aborted',
+  'entity.too.large',
+  'request.size.invalid',
+  'stream.encoding.set',
+  'stream.not.readable',
+  'parameters.too.many',
+  'charset.unsupported',
+  'encoding.unsupported',
+];
+
 // eslint-disable-next-line no-unused-vars
 export default (err: any, req: Request, res: Response, next: NextFunction) => {
   let { statusCode, message, code } = err;
 
-  if (!(err instanceof ApiError)) {
+  if (err.status && bodyParserErrorType.includes(err.type)) {
+    code = err.type.replace(/\./g, '_').toUpperCase();
+  } else if (!(err instanceof ApiError)) {
     statusCode = httpStatus.INTERNAL_SERVER_ERROR;
     message = httpStatus[httpStatus.INTERNAL_SERVER_ERROR];
     code = httpStatus[`${httpStatus.INTERNAL_SERVER_ERROR}_NAME`];
