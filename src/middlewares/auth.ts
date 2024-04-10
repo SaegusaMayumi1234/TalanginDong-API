c89560e241f2bd366fd4e47467c319a891fec31e
Theresano Julian Laksono <theresano.laksono@binus.edu>
2024-04-11 01:24:39 +0700

    fix: enhance security by using password hash as key

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,12 +2,21 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import httpStatus from 'http-status';
 
+import db from '../storages/mongoDB/index';
 import config from '../config/config';
 import ApiError from '../utils/apiError';
 
 export default () => async (req: Request, res: Response, next: NextFunction) => {
   try {
-    jwt.verify((req.headers.authorization ?? '').replace(/Bearer /, ''), config.jwt.secret);
+    const token = (req.headers.authorization ?? '').replace(/Bearer /, '');
+    const decodedPayload = JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString('utf8'));
+    const user = await db.userSchema.findById(decodedPayload.id);
+    if (!user) {
+      next(new ApiError(httpStatus.NOT_FOUND, 'User id not found'));
+      return;
+    }
+    jwt.verify(token, config.jwt.secret + user.password);
+    res.locals.user = user;
     next();
   } catch (error: any) {
     if (error instanceof jwt.JsonWebTokenError || error instanceof jwt.NotBeforeError) {
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -40,27 +40,20 @@ export const login = async (email: string, password: string) => {
   const token = jwt.sign(
     {
       id: user._id,
-      email,
-      username: user.username,
     },
-    config.jwt.secret,
+    config.jwt.secret + user.password,
     { expiresIn: config.jwt.expired },
   );
   const refreshToken = jwt.sign(
     {
       id: user._id,
-      email,
-      username: user.username,
     },
-    config.jwt.secret,
+    config.jwt.secret + user.password,
     { expiresIn: config.jwt.refreshExpired },
   );
   return {
     token,
     refreshToken,
-    id: user._id,
-    email: user.email,
-    username: user.username,
   };
 };
 
@@ -70,15 +63,17 @@ export const refreshToken = async (refreshToken: string) => {
     const decodedToken = jwt.verify(refreshToken, config.jwt.secret, { complete: true });
     decodedPayload = decodedToken.payload as JwtPayload;
   } catch (error: any) {
-    throw new ApiError(httpStatus.UNAUTHORIZED, 'Refresh Token is Invalid');
+    throw new ApiError(httpStatus.UNAUTHORIZED, 'Refresh token is invalid');
+  }
+  const user = await db.userSchema.findById(decodedPayload.id);
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User id not found');
   }
   const token = jwt.sign(
     {
       id: decodedPayload.id,
-      email: decodedPayload.email,
-      username: decodedPayload.username,
     },
-    config.jwt.secret,
+    config.jwt.secret + user.password,
     { expiresIn: config.jwt.expired },
   );
   return {
