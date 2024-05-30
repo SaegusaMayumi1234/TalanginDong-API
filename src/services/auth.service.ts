dcfcd0541305bb45d8019b6a83d584acb70d9815
Theresano Julian Laksono <theresano.laksono@binus.edu>
2024-05-30 10:54:09 +0700

    feat: Added some initial friend service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -6,6 +6,7 @@ import config from '../config/config';
 import db from '../storages/mongoDB/index';
 import ApiError from '../utils/apiError';
 
+// this function could be improved by removing find check first
 export const register = async (username: string, email: string, password: string) => {
   const users: any = await Promise.all([db.userSchema.findOne({ username }), db.userSchema.findOne({ email })]);
   if (users[0]) {
@@ -25,9 +26,8 @@ export const register = async (username: string, email: string, password: string
       } else if (errKey.includes('username')) {
         throw new ApiError(httpStatus.CONFLICT, 'Username already exist');
       }
-    } else {
-      throw error;
     }
+    throw error;
   }
 };
 
--- src/services/friend.service.ts
+++ src/services/friend.service.ts
@@ -5,9 +5,12 @@ import db from '../storages/mongoDB/index';
 import ApiError from '../utils/apiError';
 
 export const list = async (userId: string) => {
-  const friendListId = (await db.friendSchema.find({ requester: userId, accepted: true })).map(
-    (value) => new mongoose.Types.ObjectId(value.requester),
+  const friendListId = (await db.friendSchema.find({ requesterId: userId, accepted: true })).map(
+    (value) => new mongoose.Types.ObjectId(value.requesterId),
   );
+  if (friendListId.length === 0) {
+    return [];
+  }
   const friendList = await db.userSchema.find({
     _id: {
       $in: friendListId,
@@ -17,9 +20,7 @@ export const list = async (userId: string) => {
 };
 
 export const search = async (userId: string, search: string) => {
-  const friendListId = (await db.friendSchema.find({ requester: userId, accepted: true })).map(
-    (value) => new mongoose.Types.ObjectId(value.recipient),
-  );
+  const friendListId = (await db.friendSchema.find({ requesterId: userId })).map((value) => new mongoose.Types.ObjectId(value.recipientId));
   const userList = await db.userSchema
     .find({
       _id: {
@@ -31,19 +32,93 @@ export const search = async (userId: string, search: string) => {
   return userList;
 };
 
+// this function could be improved by removing find check first
 export const request = async (requesterId: string, recipientId: string) => {
   if (await db.friendSchema.find({ requester: requesterId, recipient: recipientId })) {
     throw new ApiError(httpStatus.CONFLICT, 'Already requested');
   }
-  return [];
+  try {
+    await new db.friendSchema({
+      combineId: `${requesterId}-${recipientId}`,
+      requesterId,
+      recipientId,
+      accepted: false,
+    }).save();
+  } catch (error: any) {
+    // need testing
+    if (error.code === 11000) {
+      const errKey = Object.keys(error.keyValue);
+      if (errKey.includes('combineId')) {
+        throw new ApiError(httpStatus.CONFLICT, 'Already requested');
+      }
+    }
+    throw error;
+  }
 };
 
-// export const cancel = async (requesterId: string, recipientId: string) => {};
+// this function could be improved by removing find check first
+export const cancel = async (requesterId: string, recipientId: string) => {
+  if (!(await db.friendSchema.find({ requester: requesterId, recipient: recipientId }))) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Friend data not found');
+  }
+  try {
+    await db.friendSchema.deleteOne({ requesterId, recipientId });
+  } catch (error: any) {
+    // need improvement and testing
+    if (error.code) {
+      throw error;
+    }
+    throw error;
+  }
+};
 
-// export const requestList = async (requesterId: string) => {};
+export const requestList = async (requesterId: string) => {
+  const requestListId = (await db.friendSchema.find({ requesterId, accepted: false })).map((value) => new mongoose.Types.ObjectId(value.recipientId));
+  if (requestListId.length === 0) {
+    return [];
+  }
+  const requestList = await db.userSchema.find({
+    _id: {
+      $in: requestListId,
+    },
+  });
+  return requestList;
+};
 
-// export const accept = async (requesterId: string, friendId: string) => {};
+// can be improved
+export const accept = async (requesterId: string, recipientId: string) => {
+  if (!(await db.friendSchema.find({ requester: requesterId, recipient: recipientId }))) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Friend data not found');
+  }
+  await db.friendSchema.findOneAndUpdate({ requester: requesterId, recipient: recipientId, accepted: false }, { accepted: true });
+};
 
-// export const reject = async (requesterId: string, friendId: string) => {};
+export const reject = async (requesterId: string, recipientId: string) => {
+  if (!(await db.friendSchema.find({ requester: requesterId, recipient: recipientId }))) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Friend data not found');
+  }
+  try {
+    await db.friendSchema.deleteOne({ requesterId, recipientId });
+  } catch (error: any) {
+    // need improvement and testing
+    if (error.code) {
+      throw error;
+    }
+    throw error;
+  }
+};
 
-// export const remove = async (recipientId: string) => {};
+export const remove = async (requesterId: string, recipientId: string) => {
+  if (!(await db.friendSchema.find({ requester: requesterId, recipient: recipientId }))) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Friend data not found');
+  }
+  try {
+    await db.friendSchema.deleteOne({ requesterId, recipientId });
+  } catch (error: any) {
+    // need improvement and testing
+    if (error.code) {
+      throw error;
+    }
+    throw error;
+  }
+};
