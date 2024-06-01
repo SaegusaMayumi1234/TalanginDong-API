e795d1b48da03dec1981c9f0475e8f57ad8d05f9
SaegusaMayumi1234 <theresanojulianl1234@gmail.com>
2024-06-01 11:05:27 +0700

    fix: Optimize register query

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -8,13 +8,6 @@ import ApiError from '../utils/apiError';
 
 // this function could be improved by removing find check first
 export const register = async (username: string, email: string, password: string) => {
-  const users: any = await Promise.all([db.userSchema.findOne({ username }), db.userSchema.findOne({ email })]);
-  if (users[0]) {
-    throw new ApiError(httpStatus.CONFLICT, 'Username already exist');
-  } else if (users[1]) {
-    throw new ApiError(httpStatus.CONFLICT, 'Email already exist');
-  }
-
   try {
     const hash = await bcrypt.hash(password, 10);
     await new db.userSchema({ username, email, password: hash }).save();
